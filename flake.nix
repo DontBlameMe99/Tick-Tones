3ddf42b328601ff407019f1e5bf139ddebfcdc58
DontBlameMe <dontblameme@noreply.codeberg.org>
2026-07-01 20:47:56 +0200

    chore(deps): update
    update the dependencies

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "TypeScript (Bun) dev environment";
+  description = "TypeScript dev environment";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -35,10 +35,29 @@
 
         hooks = {
           alejandra.enable = true;
+          deadnix.enable = true;
+          flake-checker.enable = true;
+          commitizen.enable = true;
+          gitlint.enable = true;
+          check-merge-conflicts.enable = true;
+          forbid-new-submodules.enable = true;
+          check-json.enable = true;
+          lychee.enable = true;
+          comrak.enable = true;
+          ripsecrets.enable = true;
+          typos.enable = true;
+          check-toml.enable = true;
+          check-yaml.enable = true;
+          check-executables-have-shebangs.enable = true;
+          check-shebang-scripts-are-executable.enable = true;
+          check-added-large-files.enable = true;
+          check-symlinks.enable = true;
+          trim-trailing-whitespace.enable = true;
+          shellcheck.enable = true;
+          woodpecker-cli-lint.enable = true;
 
-          biome-check =
-            mkHook "biome format"
-            "${pkgs.biome}/bin/biome format --write .";
+          biome.enable = true;
+          html-tidy.enable = true;
 
           typecheck =
             mkHook "tsc"
